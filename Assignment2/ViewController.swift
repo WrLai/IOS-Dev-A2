7be24d044ff9edbcd6c8311be8691ffd6ef7d712
Weiren Lai <weiren.lai@wsu.edu>
2021-02-02 23:33:53 -0800

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Weiren Lai on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
